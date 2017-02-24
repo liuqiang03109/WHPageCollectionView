1ab4c912c78581d574459f010ae4e834a720c102
wenhe-liu <liu.wen.he@chrjdt.com>
2017-02-24 14:03:46 +0800

    0.1.0

---
+++ Example/Pods/Target Support Files/WHPageCollectionView/WHPageCollectionView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WHPageCollectionView : NSObject
+@end
+@implementation PodsDummy_WHPageCollectionView
+@end
--- Example/WHPageCollectionView/ViewController.swift
+++ Example/WHPageCollectionView/ViewController.swift
@@ -7,18 +7,55 @@
 //
 
 import UIKit
+import WHPageCollectionView
+
+
+private let cellID = "cellID"
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let layout = WHPageCollectionViewLayout()
+        
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+        
+        let style = WHPageStyle()
+        style.isShowBottomLine = true
+        let frame = CGRect(x: 0, y: 20, width: view.bounds.width, height: 300)
+        
+        let page = WHPageCollectionView(frame: frame, titles: ["表情", "专属", "热门", "美女"], layout: layout, style: style, isTitleInTop: true)
+        page.dataSource = self
+        view.addSubview(page)
+        page.register(cellClass: UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+        
+        
+        
     }
+ 
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+
+
+extension ViewController: WHPageCollectionViewDataSource {
+    
+    func numberOfSections(in whPageCollectionView: WHPageCollectionView) -> Int {
+        return 4
     }
-
+    func whPageCollectionView(_ whPageCollectionView: WHPageCollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return section * 9 + 1
+    }
+    func whPageCollectionView(_ whPageCollectionView: WHPageCollectionView, _ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        
+        cell.backgroundColor = UIColor(red: CGFloat(arc4random_uniform(256)) / 255.0, green: CGFloat(arc4random_uniform(256)) / 255.0, blue: CGFloat(arc4random_uniform(256)) / 255.0, alpha: 1)
+        
+        return cell
+    }
+    
 }
 
